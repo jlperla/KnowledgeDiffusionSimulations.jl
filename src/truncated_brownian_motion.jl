d8147dd4759f2ce909569fb83d40f0a6d70882b8
Pooya Rashidi Ravari <pooyaravari@gmail.com>
2019-08-19 15:21:12 -0700

    update
    with putting `g = 10` or high enough, you can see that it would be the same as the case without `g`.

--- src/truncated_brownian_motion.jl
+++ src/truncated_brownian_motion.jl
@@ -1,46 +1,24 @@
-using DifferentialEquations ,  StochasticDiffEq, DiffEqJump, Plots
-###------------------
-g = 0.05
-function INPLACE_WHITE_NOISE_DIST_Trunc(rand_vec,W,dt,rng)
-    rand_vec_1 = (g * dt).* rand_vec
+using DifferentialEquations, StochasticDiffEq, DiffEqJump, Plots, DiffEqNoiseProcess
+function INPLACE_WHITE_NOISE_DIST_Trunc(g, rand_vec,W,dt,rng)
   DiffEqNoiseProcess.wiener_randn!(rng,rand_vec)
   rand_vec .*= sqrt(abs(dt))
-    rand_vec .= min.(rand_vec_1, rand_vec)
+    rand_vec .= min.(rand_vec, g * abs(dt))
 end
-TruncatedWienerProcess(t0,W0,Z0=nothing) = NoiseProcess(t0,W0,Z0,INPLACE_WHITE_NOISE_DIST_Trunc,
-    nothing)
-###------------------
-struct AffectIndex{F1, F2}
-affect_index!::F1
-index::F2
-end
-function (p::AffectIndex)(integrator)
-return p.affect_index!(integrator, p.index)
+TruncatedWienerProcess(g, t0,W0, Z0=nothing) = NoiseProcess(t0,W0,Z0,
+    (rand_vec,W,dt,rng) -> INPLACE_WHITE_NOISE_DIST_Trunc(g, rand_vec,W,dt,rng),nothing)
+
+function μ_SDE(u,p,t)
+    p.μ
 end
 
-####
-function μ_SDE(du,u,p,t)
-du .= p.μ
-end
+function σ_SDE(u,p,t)
+    p.σ
+end  # just draws from the inital condition
 
-function σ_SDE(du,u,p,t)
-du .= p.σ
-end
-p = (μ = 0.01, σ = 0.1, N = 5) # if all constant
-T = 10.0  # maximum time length
-x_iv = rand(p.N)  # just draws from the inital condition
-#with TruncatedWienerProcess
-w = TruncatedWienerProcess(0.0,zeros(p.N),zeros(p.N))
+p = (μ = 0.01, σ = 0.1, N = 5, g = 0.05) # if all constant
+T = 10.0
+x_iv = rand(p.N)
+w = TruncatedWienerProcess(p.g, 0.0, zeros(p.N), zeros(p.N))
 prob = SDEProblem(μ_SDE, σ_SDE, x_iv ,(0.0, T), p, noise = w)
-#without TruncatedWienerProcess
-#prob = SDEProblem(μ_SDE, σ_SDE, x_iv ,(0.0, T), p)
-
-
-rate(u,p,t) = 0.2
-affect_index!(integrator, index) = (integrator.u[index] =
-    max(integrator.u[index], integrator.u[rand(1:integrator.p.N)]) )
-jumps = [ConstantRateJump(rate,AffectIndex(affect_index!, i)) for i in 1:p.N]
-jump_prob = JumpProblem(prob,DirectFW(),JumpSet((), jumps, nothing, nothing))
-sol = solve(jump_prob, SRIW1(), adaptive=false, dt = 0.1);
-#sol = solve(jump_prob, SRIW1());#without TruncatedWienerProcess
-plot(sol)
+sol = solve(prob, EM(), dt= 0.1)
+plot(sol, label = "g = 0.05")
